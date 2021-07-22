bbbc1a9e7455a63ee960fa9365cbf697879c38ec
Alejaldo <statfluct@yahoo.com>
2021-07-22 10:28:06 +0500

    Add settings for Heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,10 +41,10 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://instatalk.herokuapp.coom/cable'
+  config.action_cable.url = 'wss://kabulchat.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [
-    'http://instatalk.herokuapp.coom/cable',
-    'https://instatalk.herokuapp.coom/cable'
+    'http://kabulchat.herokuapp.com/cable',
+    'https://kabulchat.herokuapp.com/cable'
   ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
