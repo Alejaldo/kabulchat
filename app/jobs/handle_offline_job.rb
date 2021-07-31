6839ec346655dd1791c527ae1659ae69270930b7
Alejaldo <statfluct@yahoo.com>
2021-08-01 02:32:09 +0500

    Add broadcasting from StatusChannel in HandleOfflineJob

--- app/jobs/handle_offline_job.rb
+++ app/jobs/handle_offline_job.rb
@@ -2,10 +2,9 @@ class HandleOfflineJob < ApplicationJob
   queue_as :critical
 
   def perform(user)
-
-    return if user.still_connected?
+    return if PresenceService.new(user).still_connected?
 
     user.update(online: false)
-
+    ActionCable.server.broadcast "StatusChannel", { user: user }
   end
 end
